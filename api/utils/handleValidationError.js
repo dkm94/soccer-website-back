c7ee811bd7e3f9c9e5fb84e00e39aaecb36c61eb
dkm94 <diane.mpacko@gmail.com>
2023-05-17 11:19:04 +0200

    change error objet stucture

--- api/utils/handleValidationError.js
+++ api/utils/handleValidationError.js
@@ -1,3 +1,5 @@
+const getError = require("./handleErrorMessages");
+
 const handleValidationError = (err, res) => {
   // let errors = Object.values(err.errors).map((el) => {
   //   return el.message;
@@ -10,17 +12,21 @@ const handleValidationError = (err, res) => {
   // } else {
   //   res.status(code).send({ messages: errors, fields: fields });
   // }
-  let errors = Object.values(err.errors).map((el) => {
-    return el.message;
-  });
-  let fields = Object.values(err.errors).map((el) => el.path);
+
+  // let errors = Object.values(err.errors).map((el) => {
+  //   return el.message;
+  // });
+  // let fields = Object.values(err.errors).map((el) => el.path);
+  // let code = 400;
+  // res.status(code).send({ messages: errors, fields: fields });
+
+  let errors = {};
   let code = 400;
-  // if (errors.length > 1) {
-  //   const formattedErrors = errors.join(" ");
-  //   return res.status(code).send({ messages: formattedErrors, fields: fields });
-  // } else {
-  // }
-  res.status(code).send({ messages: errors, fields: fields });
+
+  Object.keys(err.errors).forEach((key) => {
+    errors[key] = err.errors[key].message;
+  });
+  return res.status(code).send({ messages: errors, error: getError("fail") });
 };
 
 module.exports = handleValidationError;
