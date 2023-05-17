e7c0c8859d14d2f3b81a0bc11904051da319bbc4
dkm94 <diane.mpacko@gmail.com>
2023-05-17 10:55:45 +0200

    handlerrors (messages = array, instead of string)

--- api/controllers/ModController.js
+++ api/controllers/ModController.js
@@ -24,15 +24,6 @@ exports.createArticle = async (req, res, next) => {
       data: newArticle,
     });
   } catch (err) {
-    // if (error.name === "ValidationError") {
-    //   let errors = {};
-
-    //   Object.keys(error.errors).forEach((key) => {
-    //     errors[key] = error.errors[key].message;
-    //   });
-
-    //   return res.status(400).send(errors);
-    // }
     next(err);
   }
 };
--- api/utils/handleValidationError.js
+++ api/utils/handleValidationError.js
@@ -1,15 +1,26 @@
 const handleValidationError = (err, res) => {
+  // let errors = Object.values(err.errors).map((el) => {
+  //   return el.message;
+  // });
+  // let fields = Object.values(err.errors).map((el) => el.path);
+  // let code = 400;
+  // if (errors.length > 1) {
+  //   const formattedErrors = errors.join(" ");
+  //   return res.status(code).send({ messages: formattedErrors, fields: fields });
+  // } else {
+  //   res.status(code).send({ messages: errors, fields: fields });
+  // }
   let errors = Object.values(err.errors).map((el) => {
     return el.message;
   });
   let fields = Object.values(err.errors).map((el) => el.path);
   let code = 400;
-  if (errors.length > 1) {
-    const formattedErrors = errors.join(" ");
-    return res.status(code).send({ messages: formattedErrors, fields: fields });
-  } else {
-    res.status(code).send({ messages: errors, fields: fields });
-  }
+  // if (errors.length > 1) {
+  //   const formattedErrors = errors.join(" ");
+  //   return res.status(code).send({ messages: formattedErrors, fields: fields });
+  // } else {
+  // }
+  res.status(code).send({ messages: errors, fields: fields });
 };
 
 module.exports = handleValidationError;
