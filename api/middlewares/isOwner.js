d315e0e53b48080cc9538f27ee2c19a1805151fe
dkm94 <diane.mpacko@gmail.com>
2023-05-22 19:08:59 +0200

    test isOwner issue

--- api/middlewares/isOwner.js
+++ api/middlewares/isOwner.js
@@ -1,8 +1,9 @@
 const getError = require("../utils/handleErrorMessages");
 
 module.exports = (req, res, next) => {
-  if (res.locals.userId != req.params.id) {
+  if (res.locals.profileId != req.params.id) {
     res.status(401).send(getError("unauthorized"));
+    res.status(401).send(res.locals.profileId, req.params.id);
     return;
   } else {
     next();
