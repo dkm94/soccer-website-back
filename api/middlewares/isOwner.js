5b2da9c21d2fa4944db5732d0a35e540eee6db9e
dkm94 <diane.mpacko@gmail.com>
2023-05-22 19:14:38 +0200

    delete log

--- api/middlewares/isOwner.js
+++ api/middlewares/isOwner.js
@@ -3,7 +3,6 @@ const getError = require("../utils/handleErrorMessages");
 module.exports = (req, res, next) => {
   if (res.locals.profileId != req.params.id) {
     res.status(401).send(getError("unauthorized"));
-    res.status(401).send(res.locals.profileId, req.params.id);
     return;
   } else {
     next();
